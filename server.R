ec3f23bbea9a10ac8a0c785cb148bf7549558f78
chanwaiyen <cwaiyen@gmail.com>
2015-11-21 12:57:31 +0800

    Update server.R

--- server.R
+++ server.R
@@ -5,8 +5,7 @@ shinyServer(function(input, output) {
 
   output$plot <- renderPlot({
     
-    plotdata <- aggregate(data = data, Headcount ~ Worker_Type + Report_Period, FUN = sum, 
-                          subset = (Country == input$Country & (Worker_Type %in% input$Worker_Type)))
+    plotdata <- subset(data, Country == input$Country & (Worker_Type %in% input$Worker_Type))
 
     barplot(plotdata$Headcount, names.arg = plotdata$Report_Period, col = "red")
 
