d42f760481567da44aa10b48ecc0e11197fbf455
chanwaiyen <cwaiyen@gmail.com>
2015-11-21 12:07:05 +0800

    Update server.R

--- server.R
+++ server.R
@@ -2,14 +2,14 @@ library(shiny)
 data <- read.csv("Headcount.csv")
 
 shinyServer(function(input, output) {
-  
-  output$plot <- renderPlot({
 
-    plotdata <- aggregate(data = data, Headcount ~ Worker_Type + Report_Period, FUN = sum, 
-                      subset = (Country == input$Country & (Worker_Type %in% input$Worker_Type)))
+  output$plot <- renderPlot({
     
-    barplot(plotdata$Headcount, names.arg = plotdata$Report_Period)
+    plotdata <- aggregate(data = data, Headcount ~ Worker_Type + Report_Period, FUN = sum, 
+                          subset = (Country == input$Country & (Worker_Type %in% input$Worker_Type)))
+
+    barplot(plotdata$Headcount, names.arg = plotdata$Report_Period, col = "red")
 
   })
-  
+
 })
